a957966dd2f043effd3c1bb549108a7118861c19
TheFullStackFred <fredrikstromberg90@gmail.com>
2022-12-21 13:43:19 +0100

    unit test for calculateScore function

--- src/__tests__/unitTests/CalculateScore.test.ts
+++ src/__tests__/unitTests/CalculateScore.test.ts
@@ -1,4 +1,14 @@
-describe('Should show the score'),
-  () => {
-    test('')
-  }
+import { calculateScore } from '../../utilities/calculateScore'
+
+describe('calculateScore function', () => {
+  test('Calculate total score', () => {
+    //arrange
+    const difficulty = 'medium'
+    const userAnswers = [{ correct: true, answer: 'a' }]
+    const questionCountdown = 10
+    //act
+    const result = calculateScore(difficulty, userAnswers, questionCountdown)
+    //assert
+    expect(result).toBe(30)
+  })
+})
